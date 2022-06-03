82b82b21da5bb53d1e9b0ee08e3a6d5d39501cb3
caiogmrocha <caioguilherme739@gmail.com>
2022-06-02 21:42:32 -0300

    feat: init the HTTP server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,5 @@
-console.log('teste')
+const http = require('http');
+
+http
+  .createServer((request, response) => response.end('Hello World'))
+  .listen(3333, () => console.log('Server is running at http://localhost:3333'));
